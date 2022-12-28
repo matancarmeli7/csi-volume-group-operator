973676b4b927e10be4524ad0e5daf4d7ef443311
matancarmeli7 <matan.carmeli7@gmail.com>
2022-12-28 17:55:40 +0200

    add repository variable
    Signed-off-by: matancarmeli7 <matan.carmeli7@gmail.com>

--- build/ci/github_actions/setup_ci_dependencies.sh
+++ build/ci/github_actions/setup_ci_dependencies.sh
@@ -1,27 +1,28 @@
 #!/bin/bash -xe
 set +o pipefail
 
-staging_repository=${REGISTRY}${STAGING_REPOSITORY}
-prod_repository=${REGISTRY}${PROD_REPOSITORY}
 triggering_branch=$(echo $CI_ACTION_REF_NAME| sed 's|/|.|g')
 image_version=${IMAGE_VERSION}
 build_number=${BUILD_NUMBER}
 commit_hash=${GITHUB_SHA:0:7}
 specific_tag="${image_version}_b${build_number}_${commit_hash}_${triggering_branch}"
-volume_group_image_specific_tag=${staging_repository}:${specific_tag}
 
 
 if [ "$triggering_branch" == "develop" ]; then
-  volume_group_image_branch_tag=${staging_repository}:latest
+  branch_tag=latest
 else
-  volume_group_image_branch_tag=${staging_repository}:${triggering_branch}
+  branch_tag=${triggering_branch}
 fi
 
-if [ "$PRODUCTION" = true ] ; then
-  volume_group_image_branch_tag=${prod_repository}:${image_version}
+if [ "$PRODUCTION" = true ]; then
+  repository=${PROD_REPOSITORY}
+  branch_tag=${image_version}
+else
+  repository=${STAGING_REPOSITORY}
 fi
 
 #echo "volume_group_image_specific_tag=${volume_group_image_specific_tag}" >> $GITHUB_OUTPUT
 #echo "volume_group_image_branch_tag=${volume_group_image_branch_tag}" >> $GITHUB_OUTPUT
+echo "repository=${repository}" >> $GITHUB_OUTPUT
 echo "specific_tag=${specific_tag}" >> $GITHUB_OUTPUT
-echo "branch_tag=latest" >> $GITHUB_OUTPUT
+echo "branch_tag=${branch_tag}" >> $GITHUB_OUTPUT
